026e0d2950d36faba7fe75e1120d17e4f186965e
Eduar Castaño <edcasdevelopment@gmail.com>
2023-03-19 18:10:25 -0500

    ajuste en rutas

--- src/templates/sideBar.js
+++ src/templates/sideBar.js
@@ -23,10 +23,10 @@ const SideBar = () => {
             </div>
             <nav class="w-full overflow-hidden row-span-5 py-2">
                 <ul class="flex flex-col h-full p-3 overflow-auto m-2 border-t border-slate-500">
-                    <li class="p-2 w-5/6 duration-300 hover:bg-violet-400 hover:text-violet-900 rounded-full"><a href="/"> <i class="bi bi-house-door-fill mr-1"></i>Inicio</a></li>
-                    <li class="p-2 w-5/6 duration-300 hover:bg-violet-400 hover:text-violet-900 rounded-full"><a href="/#/productos"> <i class="bi bi-bag-check mr-1"></i>Productos</a></li>
-                    <li class="p-2 w-5/6 duration-300 hover:bg-violet-400 hover:text-violet-900 rounded-full"><a href="/#/ventas"> <i class="bi bi-cash-coin mr-1"></i>Ventas</a></li>
-                    <li class="p-2 w-5/6 duration-300 hover:bg-violet-400 hover:text-violet-900 rounded-full"><a href="/#/usuarios"> <i class="bi bi-people mr-1"></i>Usuarios</a></li>
+                    <li class="p-2 w-5/6 duration-300 hover:bg-violet-400 hover:text-violet-900 rounded-full"><a href="#/"> <i class="bi bi-house-door-fill mr-1"></i>Inicio</a></li>
+                    <li class="p-2 w-5/6 duration-300 hover:bg-violet-400 hover:text-violet-900 rounded-full"><a href="#/productos"> <i class="bi bi-bag-check mr-1"></i>Productos</a></li>
+                    <li class="p-2 w-5/6 duration-300 hover:bg-violet-400 hover:text-violet-900 rounded-full"><a href="#/ventas"> <i class="bi bi-cash-coin mr-1"></i>Ventas</a></li>
+                    <li class="p-2 w-5/6 duration-300 hover:bg-violet-400 hover:text-violet-900 rounded-full"><a href="#/usuarios"> <i class="bi bi-people mr-1"></i>Usuarios</a></li>
                     <li class="p-2 w-5/6 duration-300 hover:bg-violet-400 hover:text-violet-900 rounded-full"><a href="#/contact"> <i class="bi bi-telephone mr-1"></i>Contacto</a></li>
                 </ul>
             </nav>
