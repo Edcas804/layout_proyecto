c1c0b6d905d1af19fb3a1b7f09c6e5a40e78adcd
Eduar Castaño <edcasdevelopment@gmail.com>
2023-03-19 18:03:09 -0500

    release/1.0.0

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import router from './routes/index'
-import './styles/styles.css'
-import 'bootstrap-icons/font/bootstrap-icons.css'
-/**
- * Escuchando cambios en el estado del documento
- */
-window.addEventListener('load', router)
-window.addEventListener('hashchange', router);
